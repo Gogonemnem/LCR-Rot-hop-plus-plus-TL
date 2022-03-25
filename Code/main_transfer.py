dc116ab58244e3732c89d8a49a9cf62e37553414
Gonem Lau <gonemlau@gmail.com>
2022-03-25 13:56:38 +0100

    run pret, mult and ft

--- Code/main_transfer.py
+++ Code/main_transfer.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import embedding
 import utils
 from transferlearningexperimental import TL
+from test import sample
 
 
 def main():
@@ -28,8 +29,10 @@ def main():
     asp_x_test = {'asp': sentences}
     asp_y_test = {'asp': tf.one_hot(polarity+1, 3)}
 
-    doc_x_train = {'doc': 0}
-    doc_y_train = {'doc': 0}
+    import dask.dataframe as dd
+    _, x, y = dd.read_csv('ExternalData/yelp/*.csv').compute()[:3000].T.values
+    doc_x_train = {'doc': x}
+    doc_y_train = {'doc': tf.one_hot(y+1, 3)}
     doc_x_test = {'doc': 0}
     doc_y_test = {'doc': 0}
 
@@ -46,12 +49,17 @@ def main():
     # pret_y_train = {'asp': empty_asp_train, 'doc': doc_y_train}
     # pret_y_test = {'asp': empty_asp_test, 'doc': doc_y_test}
 
-    # The | operator merges the dictionaries
-    # but i believe this is not what we want
-    mult_x_train = asp_x_train | doc_x_train
-    mult_x_test = asp_x_test | doc_x_test
-    mult_y_train = asp_y_train | doc_y_train
-    mult_y_test = asp_y_test | doc_y_test
+    
+    import dask.dataframe as dd
+    df_asp = dd.read_csv('ExternalData\sem_train_2015.csv', dtype={'polarity': 'object'})
+    *asp_x, asp_y = sample(df_asp, size=3000, balance=False).compute().fillna('').T.values
+
+    df_doc: dd.DataFrame = dd.read_csv('ExternalData/yelp/*.csv', dtype={'polarity': 'object'})
+    _, doc_x, doc_y = sample(df_doc, size=1000, balance=True).compute().T.values
+    mult_x_train = {'asp': asp_x, 'doc': doc_x}
+    # mult_x_test = 
+    mult_y_train = {'asp': tf.one_hot(asp_y.astype(int)+1, 3), 'doc': tf.one_hot(doc_y.astype(int)+1, 3)}
+    # mult_y_test = asp_y_test | doc_y_test
 
     # ft_x_train = {'asp': asp_x_train, 'doc': empty_doc_train}
     # ft_x_test = {'asp': asp_x_test, 'doc': empy_doc_test}
@@ -64,14 +72,14 @@ def main():
     
     # compile at each stage because input changes
     # pret
-    # model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    # model.fit(doc_x_train, doc_y_train, validation_data=(doc_x_test, doc_y_test), epochs=200, batch_size=64)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(doc_x_train, doc_y_train, epochs=1, batch_size=32)
     # TODO: save best model
 
     # TODO: load best previous model if exists
     # # mult
     model.compile(optimizer='adam', loss='categorical_crossentropy', loss_weights = {'asp': 1, 'doc': 0.5}, metrics=['accuracy'])
-    # model.fit(mult_x_train, mult_y_train, validation_data=(mult_x_test, mult_y_test), epochs=200, batch_size=64)
+    model.fit(mult_x_train, mult_y_train, epochs=5, batch_size=32)
 
     # ft
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
--- Code/test.py
+++ Code/test.py
@@ -1,28 +1,49 @@
 import dask.dataframe as dd
 
-df: dd.DataFrame = dd.read_csv('ExternalData/yelp.csv', on_bad_lines='skip', engine="python", dtype={'polarity': 'object'})
-# print(df.info())
-# for i in range(10):
-#     print(df.sample(frac=.1).head(5))
-# df['polarity'] = df['polarity'].astype(int)
-size = 10_000
-pos: dd.DataFrame = df.loc[df['polarity'] == '1']
-pos = pos.sample(frac=(size+10)/len(pos), replace=(size+10)/len(pos)>1).head(size, -1)
-print(len(pos))
-neu: dd.DataFrame = df.loc[df['polarity'] == '0']
-neu = neu.sample(frac=(size+10)/len(neu), replace=(size+10)/len(neu)>1).head(size, -1)
-print(len(neu))
-neg: dd.DataFrame = df.loc[df['polarity'] == '-1']
-neg = neg.sample(frac=(size+10)/len(neg), replace=(size+10)/len(neg)>1).head(size, -1)
-print(len(neg))
+def sample(df: dd.DataFrame, size: int=None, balance: bool=True):
+    if balance:
+        pos: dd.DataFrame = df.loc[df['polarity'] == '1']
+        frac = 1 if size is None else size/len(pos) + 1e-5 # prevent rounding errors
+        pos = pos.sample(frac=frac, replace=frac>1).head(size, -1)
+        print('total positives', len(pos))
 
+        neu: dd.DataFrame = df.loc[df['polarity'] == '0']
+        frac = 1 if size is None else size/len(neu) + 1e-5
+        neu = neu.sample(frac=frac, replace=frac>1).head(size, -1)
+        print('total neutral', len(neu))
+
+        neg: dd.DataFrame = df.loc[df['polarity'] == '-1']
+        frac = 1 if size is None else size/len(neg) + 1e-5
+        neg = neg.sample(frac=frac, replace=frac>1).head(size, -1)
+        print('total negatives', len(neg))
+
+        sample = dd.concat([pos, neu, neg]).sample(frac=1)
+    else:
+        frac = 1 if size is None else size/len(df) + 1e-5
+        sample = df.sample(frac=frac, replace=frac>1)
+        
+    print('total sample', len(sample))
+    return sample
+        
+
+# df: dd.DataFrame = dd.read_csv('ExternalData/yelp.csv', on_bad_lines='skip', engine="python", dtype={'polarity': 'object'})
+# size = 10_000
+# pos: dd.DataFrame = df.loc[df['polarity'] == '1']
+# pos = pos.sample(frac=(size+10)/len(pos), replace=(size+10)/len(pos)>1).head(size, -1)
+# print(len(pos))
 # neu: dd.DataFrame = df.loc[df['polarity'] == '0']
-# neu = neu.sample(frac=(size+10)/len(neu), replace=(size+10)/len(neu)>1).reset_index().loc['0': f'{size}']
+# neu = neu.sample(frac=(size+10)/len(neu), replace=(size+10)/len(neu)>1).head(size, -1)
+# print(len(neu))
 # neg: dd.DataFrame = df.loc[df['polarity'] == '-1']
-# neg = neg.sample(frac=(size+10)/len(neg), replace=(size+10)/len(neg)>1).reset_index().loc['0': f'{size}']
+# neg = neg.sample(frac=(size+10)/len(neg), replace=(size+10)/len(neg)>1).head(size, -1)
+# print(len(neg))
 
-sample = dd.concat([pos, neu, neg]).sample(frac=1)
-print(len(sample))
+# sample = dd.concat([pos, neu, neg]).sample(frac=1)
+# print(len(sample))
 
-sample.to_csv('ExternalData/yelp/yelp-*.csv')  
+# sample.to_csv('ExternalData/yelp/yelp-*.csv')  
 
+# df: dd.DataFrame = dd.read_csv('ExternalData/yelp/*.csv')
+# print(df.head(5))
+# print(len(df))
+# print(df.compute().T.values)
